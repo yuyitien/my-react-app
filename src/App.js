4380e0162f665c8c71acc5c74f17d55e709ad521
yuyitien <5b0g0007@office.stust.edu.tw>
2024-06-04 21:30:13 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import MyRGBPanel from "./components/MyRGBPanel";
-import MySlider from "./components/MySlider";
-import MyCalculator from "./components/MyCalculator";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel"
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <MyRGBPanel />
       <h1>MyCalculator</h1>
       <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
     </div>
   );
 }
