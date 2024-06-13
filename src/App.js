bd6bd266080c1054a7b920d22086ac05cf5f9b13
yuyitien <5b0g0007@office.stust.edu.tw>
2024-06-13 21:44:30 +0800

    0613

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MySlider from "./MySlider";
+import MyRGBPanel from "./components/MyRGBPanel";
+import MySlider from "./components/MySlider";
 
 function App() {
   return (
     <div className="App">
-      <h1>Javascript程式設計，使用react</h1>
-      <h2>第一個元件</h2>
-      <span>R: </span><MySlider />
-      <span>G: </span><MySlider />
-      <span>B: </span><MySlider />
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React! 2024.5.1</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-      {/* </header> */}
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
     </div>
   );
 }
