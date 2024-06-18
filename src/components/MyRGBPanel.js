d5f7a67f284284d6c81fdfb40c6a23069bb373ac
yuyitien <5b0g0007@office.stust.edu.tw>
2024-06-18 22:39:14 +0800

    1111

--- src/components/MyRGBPanel.js
+++ src/components/MyRGBPanel.js
@@ -42,12 +42,12 @@ function MyRGBPanel() {
             backgroundColor: `rgb(${r}, ${g}, ${b})`, // 根據 r, g, b 值設置背景顏色
           }}
         />
-        <span>R: </span> /* 紅色滑動條標籤 */
-        <MySlider onChange={updateR} /> --MySlider 組件，當值改變時調用 updateR
-        <span>G: </span> // 綠色滑動條標籤
-        <MySlider onChange={updateG} /> // MySlider 組件，當值改變時調用 updateG
-        <span>B: </span> // 藍色滑動條標籤
-        <MySlider onChange={updateB} /> // MySlider 組件，當值改變時調用 updateB
+        <span>R: </span> 
+        <MySlider onChange={updateR} /> 
+        <span>G: </span> 
+        <MySlider onChange={updateG} /> 
+        <span>B: </span>
+        <MySlider onChange={updateB} /> 
       </div>
     </>
   );
